6354f7371a1b94f54f00b37aceea79aeedd9f67f
Rafael Cardenas <cardenas.rfl@gmail.com>
2013-04-02 23:07:20 -0700

    soporte para notas y unidades

--- inegi_sql.py
+++ inegi_sql.py
@@ -2,7 +2,7 @@
 
 """Parser INEGI
 
-Recibe un TSV de informacion nacional y lo transmite hacia
+Recibe un TSV de informacion del INEGI y lo transmite hacia
 una base de datos PostgreSQL.
 http://www3.inegi.org.mx/sistemas/descarga/default.aspx?c=28088
 
@@ -37,13 +37,16 @@ class INEGIParser(object):
       nombre varchar);")
     self.sql.execute("CREATE TABLE IF NOT EXISTS indicador (\
       id bigint PRIMARY KEY,\
-      descripcion varchar);")
+      descripcion varchar,\
+      notas varchar);")
     self.sql.execute("CREATE TABLE IF NOT EXISTS valor (\
       indicador bigint references indicador(id),\
       municipio varchar(3),\
       entidad varchar(2),\
       anio integer,\
-      valor numeric(20,5));")
+      valor numeric(20,5),\
+      unidades varchar,\
+      fuente varchar);")
     self.sql.execute("CREATE TABLE IF NOT EXISTS categoria (\
       id serial,\
       nombre varchar PRIMARY KEY,\
@@ -59,6 +62,8 @@ class INEGIParser(object):
   def parse(self):
     # path = "00_NacionalyEntidadesFederativas"
     path = "01_Aguascalientes"
+    # el csv de nacional no trae las columnas de los anios como las estatales,
+    # y no son provistos en ningun archivo
     anio = [1895,1900,1910,1921,1930,1940,1950,1960,1970,1980,1981,1982,1983,
       1984,1985,1986,1987,1988,1989,1990,1991,1992,1993,1994,1995,1996,1997,
       1998,1999,2000,2001,2002,2003,2004,2005,2006,2007,2008,2009,2010]
@@ -68,10 +73,10 @@ class INEGIParser(object):
       "datos/%s_tsv/%s_Valor.tsv" % (path, path)], stdout=subprocess.PIPE)
     self.encoding = fenc.stdout.read().split(':')[1].strip()
 
-    print "Procesando CSV de valores..."
+    print "Procesando CSV 1/4..."
     with open("datos/%s_tsv/%s_Valor.tsv" % (path, path)) as inegi_tsv:
       for l, line in enumerate(csv.reader(inegi_tsv, dialect="excel-tab")):
-        if l == 0 and (not line[0].isdigit()):
+        if l == 0 and (not line[0].isdigit()): # corregir si viene con columnas
           continue
         self.wentidad(line[0],line[1])
         self.wmunicipio(line[0],line[2],line[3])
@@ -81,21 +86,26 @@ class INEGIParser(object):
         self.wcategoria(line[6],line[5])
         for i, a in enumerate(line[9:]):
           if a:
-            self.wvalor(int(line[7]),line[2],line[0],anio[i],float(a))
+            self.wvalor(line[7],line[2],line[0],anio[i],a)
 
-    print "Procesando CSV de notas..."
-    # with open('%s_tsv/%s_Notas.tsv' % (path, path)) as inegi_tsv:
-    #   for l, line in enumerate(csv.reader(inegi_tsv, dialect="excel-tab")):
-    #     if l == 0:
-    #       continue
-    #     # TODO: hay lineas que empiezan sin ID, son linebreaks de la anterior.
-    #     # ademas hay algunos IDs repetidos que su desc debe ser concatenada
-    #     if line[0].isdigit():
-    #       self.sql.execute("SELECT * FROM indicador WHERE id=%s",(int(line[0]),))
-    #       previous = self.sql.fetchone()
-    #       if not previous:
-    #         self.sql.execute("INSERT INTO notas (indicador,notas) VALUES (%s,%s);",
-    #           (int(line[0]),line[2]))
+    print "Procesando CSV 2/4..."
+    with open('datos/%s_tsv/%s_Notas.tsv' % (path, path)) as inegi_tsv:
+      for l, line in enumerate(csv.reader(inegi_tsv, dialect="excel-tab")):
+        if l == 0: # ignorar nombres de columnas
+          continue
+        # TODO: hay lineas que empiezan sin ID, son linebreaks de la anterior.
+        # ademas hay algunos IDs repetidos que su desc debe ser concatenada
+        if line[0].isdigit():
+          self.wnota(line[0],line[2])
+
+    print "Procesando CSV 3/4..."
+    with open("datos/%s_tsv/%s_UnidadMedida.tsv" % (path, path)) as inegi_tsv:
+      for l, line in enumerate(csv.reader(inegi_tsv, dialect="excel-tab")):
+        if l == 0 and (not line[0].isdigit()): # corregir si viene con columnas
+          continue
+        for i, a in enumerate(line[9:]):
+          if a:
+            self.wunidades(line[7],line[2],line[0],anio[i],a)
 
   def strdecode(self, s):
     if not self.encoding == "utf-8":
@@ -106,7 +116,12 @@ class INEGIParser(object):
   def wvalor(self, indicador, municipio, entidad, anio, valor):
     self.sql.execute("INSERT INTO valor \
       (indicador,municipio,entidad,anio,valor) VALUES (%s,%s,%s,%s,%s);", 
-      (indicador,municipio,entidad,anio,valor))
+      (int(indicador),municipio,entidad,anio,float(valor)))
+
+  def wunidades(self, indicador, municipio, entidad, anio, unidades):
+    self.sql.execute("UPDATE valor SET unidades=%s WHERE indicador=%s \
+      AND municipio=%s AND entidad=%s AND anio=%s;", 
+      (unidades,int(indicador),municipio,entidad,anio))
 
   def wentidad(self, eid, nombre):
     try:
@@ -128,6 +143,10 @@ class INEGIParser(object):
         (%s,%s);", (int(iid),self.strdecode(descripcion)))
     except Exception, e:
       pass
+
+  def wnota(self, iid, notas):
+    self.sql.execute("UPDATE indicador SET notas=%s WHERE id=%s;", 
+      (self.strdecode(notas),int(iid)))
 
   def wcategoria(self, nombre, parent):
     if nombre:
